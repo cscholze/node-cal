1fac311ab8074bcc193ca80f76a4b7a78a6fa0cf
cScholze <caleb.scholze@gmail.com>
2016-01-27 14:57:09 -0600

    added function to create and center calendar header

--- cal.js
+++ cal.js
@@ -1,5 +1,47 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --harmony_destructuring
 
+// use ES6 strict mode
 'use strict';
 
-console.log('Welcome to jCal!');
+const Month = require('./lib/month');
+const Year = require('./lib/year');
+
+
+// get command line arguments
+// const args = process.argv.slice(2);
+const [,, ...args] = process.argv;
+
+
+console.log('args: ', args);
+
+
+if (args.length === 0) {
+  let today = new Date();
+
+  // month is (0-11) converted to (1-12)
+  let month = today.getMonth() + 1;
+
+  // (1753-9999)
+  let year = today.getFullYear();
+
+  // day is (1-31);
+  let day = today.getDate();
+
+  console.log(`Current date = ${month} ${day}, ${year}`);
+  Month.generateMonth(year, month);
+}
+else if (args.length === 2) {
+  const [month, year] = args;
+
+  console.log(`generateMonth(${year}, ${month})`);
+}
+else if (args.length === 1) {
+  const[year] = args;
+
+  console.log(`generateYear(${year})`);
+}
+else {
+  console.log('invalid args.length');
+  // set error code for CLI app
+  // process.exit(64);
+}
